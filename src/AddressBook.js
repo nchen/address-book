19ec09e7d5457e2245c62521250cf7d065020db6
woodfox <woodfox@wfmbp.local>
2020-05-24 20:52:06 +0800

    Replace default alert with Bootstrap Dialog

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Table, Button, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Table, Button, Row, Col, Modal } from "react-bootstrap";
 import addressData from "./addressData.json";
 import { initialState, addressBookReducer } from "./addressBookReducer";
 import { useImmerReducer } from "use-immer";
@@ -7,30 +7,37 @@ import { produce } from "immer";
 
 const _initialState = { ...initialState, data: addressData };
 
-const doUpdate = (state) => {
-  // Exclude deleted rows for updating.
-  let editedCellphones = produce(state.editedCellphones, (draft) => {
-    for (let id in draft) {
-      const rowId = parseInt(id);
-      if (state.deletedRowIds.indexOf(rowId) > -1) {
-        delete draft[id];
-      }
-    }
-  });
-
-  const updateData = {
-    deletedRowIds: state.deletedRowIds,
-    editedCellphones,
-  };
-  alert("Updating data: \n" + JSON.stringify(updateData));
-};
-
 const AddressBook = () => {
   let [state, dispatch] = useImmerReducer(addressBookReducer, _initialState);
+  let [showDialog, setShowDialog] = useState(false);
+  let [msg, setMsg] = useState("");
+  const handleClose = () => {
+    setShowDialog(false);
+  };
+
+  const doUpdate = (state) => {
+    // Exclude deleted rows for updating.
+    let editedCellphones = produce(state.editedCellphones, (draft) => {
+      for (let id in draft) {
+        const rowId = parseInt(id);
+        if (state.deletedRowIds.indexOf(rowId) > -1) {
+          delete draft[id];
+        }
+      }
+    });
+
+    const updateData = {
+      deletedRowIds: state.deletedRowIds,
+      editedCellphones,
+    };
+    setMsg("Updating data: \n" + JSON.stringify(updateData));
+    setShowDialog(true);
+  };
 
   return (
     <div>
       <h1>Address Book</h1>
+
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -150,7 +157,11 @@ const AddressBook = () => {
           </Button>
         </Col>
         <Col className="text-right p-5">
-          <Button variant="warning" className="mx-5" onClick={() => doUpdate(state)}>
+          <Button
+            variant="warning"
+            className="mx-5"
+            onClick={() => doUpdate(state)}
+          >
             Update
           </Button>
           <Button variant="warning" onClick={() => dispatch({ type: "add" })}>
@@ -158,6 +169,18 @@ const AddressBook = () => {
           </Button>
         </Col>
       </Row>
+
+      <Modal show={showDialog} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Alert</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{msg}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
